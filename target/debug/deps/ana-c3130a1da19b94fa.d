b06d8e1dd5af0c4d5612fe02d6d883cd16f7b8c3
bsg <bsg>
2025-10-10 14:07:14 -0400

    oops added target to git by mistake. removed now.

--- target/debug/deps/ana-c3130a1da19b94fa.d
+++
@@ -1,9 +0,0 @@
-/home/luca/projects/rust/ana/target/debug/deps/ana-c3130a1da19b94fa.d: src/main.rs src/toml/mod.rs src/toml/data/mod.rs src/toml/parser/mod.rs src/toml/scanner/mod.rs
-
-/home/luca/projects/rust/ana/target/debug/deps/libana-c3130a1da19b94fa.rmeta: src/main.rs src/toml/mod.rs src/toml/data/mod.rs src/toml/parser/mod.rs src/toml/scanner/mod.rs
-
-src/main.rs:
-src/toml/mod.rs:
-src/toml/data/mod.rs:
-src/toml/parser/mod.rs:
-src/toml/scanner/mod.rs:
--- target/debug/deps/ana-f17a397b6d3149f4.d
+++
@@ -1,9 +0,0 @@
-/home/luca/projects/rust/ana/target/debug/deps/ana-f17a397b6d3149f4.d: src/main.rs src/toml/mod.rs src/toml/data/mod.rs src/toml/parser/mod.rs src/toml/scanner/mod.rs
-
-/home/luca/projects/rust/ana/target/debug/deps/libana-f17a397b6d3149f4.rmeta: src/main.rs src/toml/mod.rs src/toml/data/mod.rs src/toml/parser/mod.rs src/toml/scanner/mod.rs
-
-src/main.rs:
-src/toml/mod.rs:
-src/toml/data/mod.rs:
-src/toml/parser/mod.rs:
-src/toml/scanner/mod.rs:
